cd986c6b53e299f369500d4c5fb884d754a499c8
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-05-02 17:11:45 -0300

    .
    Initial Commit

---
+++ scrollReaveal.js
@@ -0,0 +1,11 @@
+
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('#container',{ duration: 2000 });
+
+sr.reveal('#service',{ duration: 2000 });
+sr.reveal('.card',{ duration: 2000 });
+sr.reveal('#cartaoFidelidade',{ duration: 2000 });
+sr.reveal('.funcionalidades',{ duration: 2000 });
+sr.reveal('.imagemMotorista',{ duration: 2000 });
+sr.reveal('.textoMotorista',{ duration: 2000 });
