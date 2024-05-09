e3528efcf6f965235d9d4e4bfd66afa5b52cea94
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-05-09 11:05:38 -0300

    Update scrollReaveal.js

--- scrollReaveal.js
+++ scrollReaveal.js
@@ -12,9 +12,9 @@ sr.reveal('.textoMotorista',{ duration: 1000 });
 sr.reveal('.passageiroCard',{ duration: 1000 });
 sr.reveal('.text',{ duration: 1000 });
 sr.reveal('.img',{ duration: 1000 });
-sr.reveal('.imagemMotorista',{ duration: 2000 });
-sr.reveal('.textPassageiro',{ duration: 2000 });
-sr.reveal('.sobre',{ duration: 3000 });
-sr.reveal('#img',{ duration: 3000 });
+sr.reveal('.imagemMotorista',{ duration: 1000 });
+sr.reveal('.textPassageiro',{ duration: 1000 });
+sr.reveal('.sobre',{ duration: 1000 });
+sr.reveal('#img',{ duration: 1000 });
 
-sr.reveal('.botaoDownload',{ duration: 3000 });
+sr.reveal('.botaoDownload',{ duration: 1000 });
