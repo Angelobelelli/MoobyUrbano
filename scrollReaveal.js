c31870039f94009bb2f40c6ec5215158e08860f1
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-05-15 15:43:28 -0300

    Mudança no Layout do site

--- scrollReaveal.js
+++ scrollReaveal.js
@@ -3,18 +3,19 @@ window.sr = ScrollReveal({ reset:true });
 
 
 
-sr.reveal('#service',{ duration: 2000 });
-sr.reveal('.card',{ duration: 2000 });
-sr.reveal('#cartaoFidelidade',{ duration: 2000 });
-sr.reveal('.funcionalidades',{ duration: 2000 });
-sr.reveal('.imagemMotorista',{ duration: 2000 });
+sr.reveal('#service',{ duration: 1000 });
+sr.reveal('.card',{ duration: 1000 });
+sr.reveal('#cartaoFidelidade',{ duration: 1000 });
+sr.reveal('.funcionalidades',{ duration: 1000 });
+sr.reveal('.imagemMotorista',{ duration: 1000 });
 sr.reveal('.textoMotorista',{ duration: 1000 });
 sr.reveal('.passageiroCard',{ duration: 1000 });
 sr.reveal('.text',{ duration: 1000 });
+sr.reveal('#celularMotorista',{ duration: 1000 });
+sr.reveal('.botaoDownload',{ duration: 1000 });
 sr.reveal('.img',{ duration: 1000 });
 sr.reveal('.imagemMotorista',{ duration: 1000 });
 sr.reveal('.textPassageiro',{ duration: 1000 });
 sr.reveal('.sobre',{ duration: 1000 });
 sr.reveal('#img',{ duration: 1000 });
 
-sr.reveal('.botaoDownload',{ duration: 1000 });
